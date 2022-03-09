44ae6f1dc690bb74b7516fc5b9ebc43bbe30aacd
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-09 13:36:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -205,7 +205,8 @@ const App = () => {
             </tr>
             {data.map(function(object, i){
               //console.log(object);
-                return <tr><td><VideoPlayer { ...object  }/></td>/tr>;
+              return <tr><td><VideoPlayer { ...object  }/></td><td><img src={thumbs_up} alt="Thumbs Up" onClick={() => populateDate('hnvasa@gmail.com',object.sources[0].src,'upvote')} /><img src={thumbs_down} alt="Thumbs Down" onClick={() => populateDate('hnvasa@gmail.com',object.sources[0].src,'downvote')} /></td></tr>;
+            })}
             })}
           </tbody>
         </table>
